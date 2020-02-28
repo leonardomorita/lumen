25db4b837baea35c429d091bdf71cc068df9d407
leonardomorita <leonardomorita2@gmail.com>
2020-02-27 23:10:35 -0300

    Adicionando a funcionalidade de atualizar um registro

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -17,12 +17,24 @@ class SeriesController
         return response()->json(Serie::create(['nome' => $requisicao->nome]), 201);
     }
 
-    public function get($id)
+    public function show($id)
     {
         $serie = Serie::find($id);
         if (is_null($serie)) {
             return response()->json('', 204);
         }
         return response()->json($serie);
+    }
+
+    public function update(int $id, Request $requisicao)
+    {
+        $serie = Serie::find($id);
+        if (is_null($serie)) {
+            return response()->json(['mensagem' => 'Recurso não encontrado'], 404);
+        }
+        $serie->fill($requisicao->all()); // preenche a série selecionada com todas as informações passados pelo parâmetro do método fill().
+        $serie->save();
+
+        return $serie;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,6 @@ $router->get('/', function () use ($router) {
 $router->group(['prefix' => '/api'], function () use ($router) {
     $router->get('/series', "SeriesController@index");
     $router->post('/series', "SeriesController@store");
-    $router->get('/series/{id}', "SeriesController@get");
+    $router->get('/series/{id}', "SeriesController@show");
+    $router->put('/series/{id}', "SeriesController@update"); // atualizar
 });
